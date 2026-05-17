78c3f93a099ca2c5cdab6da3b0af6a1c114aa31a
WolFerinus <eduardomartins0706@gmail.com>
2026-05-17 14:24:07 -0300

    Desafio sem test-id

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,15 +1,26 @@
 import { test, expect } from '@playwright/test';
 
+///AAA - Arrange, Act, Assert
+
+
 test('Deve consultar um pedido aprovado', async ({ page }) => {
+    //Arrange - Preparar o cenário, definir as variáveis
+    const orderId = 'VLO-3IS83F';
     await page.goto('http://localhost:5173/');
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
     await page.getByRole('link', { name: 'Consultar Pedido' }).click();
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+    //Act - Agir, interagir com a página
     await expect(page.getByTestId('search-order-id')).toBeVisible();
-    await page.getByTestId('search-order-id').fill('VLO-3IS83F');
-    await page.getByTestId('search-order-button').click();
-    await expect(page.getByTestId('order-result-id')).toBeVisible();
-    await expect(page.getByTestId('order-result-id')).toContainText('VLO-3IS83F');
-    await expect(page.getByTestId('order-result-status')).toBeVisible();
-    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+    await page.getByTestId('search-order-id').fill(orderId);
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+    //Assert - Verificar o resultado
+    await expect(page.getByTestId(`order-result-${orderId}`)).toBeVisible({ timeout: 6000 });
+    await expect(page.getByTestId(`order-result-${orderId}`)).toContainText(orderId);
+    await expect(page.getByTestId(`order-result-${orderId}`)).toBeVisible();
+    await expect(page.getByTestId(`order-result-${orderId}`)).toContainText('APROVADO');
 });
+
+
